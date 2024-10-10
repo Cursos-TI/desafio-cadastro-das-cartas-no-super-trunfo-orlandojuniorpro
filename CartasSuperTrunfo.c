a709b8cda751306fd85762ffc9d910fd76f3708e
orlandojuniorpro <orlandojuniorpro@gmail.com>
2024-10-10 22:00:56 +0000

    Adicionei um comentário

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,7 +17,11 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    // Países: Brasil, Holanada, EUA e Reino Unido
+
     printf("Desafio Cartas Super Trunfo!\n");
+
+    
     
 
     return 0;
