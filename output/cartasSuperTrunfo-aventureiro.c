01d0a52025419af4b9369dd4e83e0558d791729c
orlandojuniorpro <orlandojuniorpro@gmail.com>
2024-10-19 16:38:09 +0000

    comecei

---
+++ output/cartasSuperTrunfo-aventureiro.c
@@ -0,0 +1,23 @@
+#include  <stdio.h>
+
+// Função genérica para entradaDados
+void entradaDados() {
+   // código para a função de entrada de dados
+}
+
+// Função genérica para calculoMedia
+float calcularMedia() {
+    // código para a função calcularMedia
+}
+ 
+// função genérica para exibição de dados
+void exibirMedia(){
+// código para a função exibirMedia
+}
+
+int main(){
+    entradaDados();
+    float media = calcularMedia();
+    exibirMedia();
+    return 0;
+    }
